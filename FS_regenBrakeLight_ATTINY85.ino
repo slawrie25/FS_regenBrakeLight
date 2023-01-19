b61c9b894d4555d14ab42d9f8c445a02e9ef6ebb
Struan Lawrie <72400814+slawrie25@users.noreply.github.com>
2023-01-19 18:17:37 +0000

    Update FS_regenBrakeLight_ATTINY85.ino
    Correct version uploaded

--- FS_regenBrakeLight_ATTINY85.ino
+++ FS_regenBrakeLight_ATTINY85.ino
@@ -1,22 +1,40 @@
-#include <USI_TWI_Master.h>
-#include <TinyWireM.h>
+/*
+* @Author Struan Lawrie
+*
+* Requires ATTinyCore board files NOT standard ATTiny board files
+* https://github.com/SpenceKonde/ATTinyCore/releases/tag/1.5.2
+* 
+* Currently using 0x1D for MMA8451 address, A pin is tied to ground
+* 
+* Wiring is as follows:
+* ATTiny pin:
+* 
+* #1(RESET/A0)
+* #2(A3)
+* #3(A2)
+* #4(GND)-----------------GND--------------PWR_GND
+* #5(SDA/0)---------------MMA_SDA
+* #6(1)-------------------LED
+* #7(SCL/A1)--------------MMA_SCL
+* #8(VCC)-----------------MMA_VCC----------PWR_5V
+*                         MMA_A------------GND
+*/
 
-
-//#include <Wire.h>
+#include <Wire.h>
 #include <Adafruit_MMA8451.h>
 #include <Adafruit_Sensor.h>
 
 #define LED 1
 
-//negative used due to direction of accelerometer
+//negative used due to direction of accelerometer (<-X)
 #define upperBound -1.3
 #define lowerBound -0.7
 
 Adafruit_MMA8451 mma = Adafruit_MMA8451();
 
 float X;
-float Y;
-float Z;
+//float Y;
+//float Z;
 
 int threshhold;
 float avgX = 0;
@@ -24,16 +42,29 @@ int samples = 10;
 
 void setup() {
 
+  //flash LED once to show power on
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+
+  //attempt to connect to MMA8451
   if (! mma.begin()) {
     while (1);
   }
 
-  mma.setRange(MMA8451_RANGE_2_G);
+  //set range of accelerometer to 4G
+  mma.setRange(MMA8451_RANGE_4_G);
 
-  digitalWrite(LED, LOW);
-  delay(500);
+  //flash LED twice to show successful accelerometer connection
   digitalWrite(LED, HIGH);
-  delay(500);
+  delay(250);
+  digitalWrite(LED, LOW);
+  delay(250);
+  digitalWrite(LED, HIGH);
+  delay(250);
+  digitalWrite(LED, LOW);
+  delay(250);
 }
 
 void loop() {
@@ -41,9 +72,10 @@ void loop() {
   sensors_event_t event;
   mma.getEvent(&event);
 
+  //variable to store acceleration value
   X = event.acceleration.x;
-  Y = event.acceleration.y;
-  Z = event.acceleration.z;
+  //Y = event.acceleration.y;
+  //Z = event.acceleration.z;
 
   //take an average for more accurate data
   avgX = 0;
